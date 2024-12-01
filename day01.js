97d84b97be75f3cf1921bcfff38854290fa78c92
Manuel Sgobino <msgobino@netcologne.de>
2024-12-01 06:24:46 +0100

    day 01 cleaned up

--- day01.js
+++ day01.js
@@ -1,24 +1,18 @@
 'use strict'
 
-const parseInput = input => {
-    return input.split('\n').map(l => l.match(/\d+/g).map(Number))
-}
-
-const solve = (isPart2, input) => {
-    return input
-}
+const parseInput = input => input.split('\n')
+    .map(l => l.match(/\d+/g).map(Number))
+    .reduce(([lc, rc], [l, r]) => [[...lc, l], [...rc, r]], [[], []])
 
 const part1 = input => {
-    input = parseInput(input)
-    let left = input.map(r => r[0]).sort()
-    let right = input.map(r => r[1]).sort()
+    let [left, right] = parseInput(input)
+    left.sort()
+    right.sort()
     return left.reduce((acc, cur, i) => acc + Math.abs(cur - right[i]), 0)
 }
 
 const part2 = input => {
-    input = parseInput(input)
-    let left = input.map(r => r[0])
-    let right = input.map(r => r[1])
+    let [left, right] = parseInput(input)
     return left.reduce((acc, cur) => acc + (cur * right.filter(f => f == cur).length), 0)
 }
 
