ae875d11eee8cafbaaa2584517d069b90125505e
Manuel Sgobino <msgobino@netcologne.de>
2024-12-04 08:56:10 +0100

    day 04 cleaned up

--- day04.js
+++ day04.js
@@ -14,33 +14,27 @@ const countXmas = (r, c, [dr, dc], grid) => {
     return 1
 }
 
-const part1 = input => {
+const solve = (isPart2, input) => {
     input = parseInput(input)
     let total = 0
 
     for (let r = 0; r < input.length; r++) {
-        for (let c = 0; c < input.length; c++) {
-            for (const delta of eightWayDeltas) total += countXmas(r, c, delta, input)
-        }
-    }
-    return total
-}
-
-const part2 = input => {
-    input = parseInput(input)
-    let total = 0
-
-    for (let r = 1; r < input.length - 1; r++) {
-        for (let c = 1; c < input.length - 1; c++) {
-            if (input[r][c] == 'A') {
-                const [tl, tr, bl, br] = getSurroundingGridTiles(input, r, c, [[-1, -1], [-1, 1], [1, -1], [1, 1]])
-                if (((tl == 'M' && br == 'S') || (tl == 'S' && br == 'M'))
-                    &&
-                    ((tr == 'M' && bl == 'S') || (tr == 'S' && bl == 'M'))) total++
+        for (let c = 0; c < input[0].length; c++) {
+            if (isPart2) {
+                if (input[r][c] == 'A') {
+                    const corners = getSurroundingGridTiles(input, r, c, [[-1, -1], [-1, 1], [1, -1], [1, 1]])
+                    total += ['MMSS', 'SMSM', 'SSMM', 'MSMS'].includes(corners.join('')) ? 1 : 0
+                }
+            } else {
+                for (const delta of eightWayDeltas) total += countXmas(r, c, delta, input)
             }
         }
     }
     return total
 }
+
+const part1 = input => solve(false, input)
+
+const part2 = input => solve(true, input)
 
 module.exports = { part1, part2 }
