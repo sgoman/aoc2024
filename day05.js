df88094b4c811d2ecbac69d8481ef434d42ae92e
Manuel Sgobino <msgobino@netcologne.de>
2024-12-05 07:23:08 +0100

    day 05 cleaned up

--- day05.js
+++ day05.js
@@ -1,41 +1,26 @@
 'use strict'
 
-const parseInput = input => {
-    const [first, second] = input.split('\n\n')
-    const rules = first.split('\n').map(l => l.match(/\d+/g).map(Number))
-    const updates = second.split('\n').map(l => l.split(',').map(Number))
-    return [rules, updates]
-}
+const parseInput = input => input.split('\n\n').map(block => block.split('\n').map(line => line.match(/\d+/g).map(Number)))
 
 const solve = (isPart2, input) => {
     const [rules, updates] = parseInput(input)
-    let total = 0
-    for (const update of updates) {
+    return updates.reduce((total, update) => {
         const relevant = rules.filter(([left, right]) => update.includes(left) && update.includes(right))
         if (relevant.every(([left, right]) => update.indexOf(left) < update.indexOf(right))) {
-            if (!isPart2) {
-                const middle = update[update.length >> 1]
-                total += middle
-            }
+            if (!isPart2) total += update[update.length >> 1]
         } else if (isPart2) {
-            console.log({update, relevant})
-            const orig = update.join(',')
             update.sort((a, b) => {
                 let rule = relevant.find(r => r.includes(a) && r.includes(b))
                 return rule.indexOf(a) - rule.indexOf(b)
             })
             total += update[update.length >> 1]
         }
-    }
-    return total
+        return total
+    }, 0)
 }
 
-const part1 = input => {
-    return solve(false, input)
-}
+const part1 = input => solve(false, input)
 
-const part2 = input => {
-    return solve(true, input)
-}
+const part2 = input => solve(true, input)
 
 module.exports = { part1, part2 }
