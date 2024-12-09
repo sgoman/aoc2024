b9c2622ac907fa3527be526ea1f976ead8c93144
Manuel Sgobino <msgobino@netcologne.de>
2024-12-09 07:10:28 +0100

    day09 challenge commit

---
+++ day09.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const parseInput = input => input.split('').map(Number).reduce((a, c, i) => [...a, [i, c]], [])
+
+const solve = (isPart2, input) => {
+    input = parseInput(input)
+    let i = input[0][1], sum = 0
+    console.log({input, i})
+    input.shift()
+    while (input.length) {
+        if (input[0][0] % 2 == 1) {
+            let [_, e] = input.shift()
+            let fi, fl
+            while (e > 0) {
+                do {[fi, fl] = input.pop()} while(fi % 2 == 1)
+                console.log({i, sum, e, fi, fl})
+                while (fi % 2 == 0 && e && fl) {
+                    sum += i++ * (fi >> 1)
+                    e--
+                    fl--
+                }
+                if (input.length == 0) break
+            }
+            if (fl) input.push([fi, fl])
+        } else {
+            let [fi, fl] = input.shift()
+            console.log({i, sum, fi, fl})
+            while (fl--) sum += i++ * (fi >> 1)
+        }
+    }
+    return sum
+}
+
+const part1 = input => solve(false, input)
+
+const part2 = input => solve(true, input)
+
+module.exports = { part1, part2 }
