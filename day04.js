2a252c71fc0fa654b6d86c52a5bb7a398a5489d0
Manuel Sgobino <msgobino@netcologne.de>
2024-12-10 21:24:21 +0100

    day 04: refactored using the new gridCells function from utils.js

--- day04.js
+++ day04.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { eightWayDeltas, validCoordForGrid, getSurroundingGridTiles } = require('./utils.js')
+const { eightWayDeltas, validCoordForGrid, getSurroundingGridTiles, gridCells } = require('./utils.js')
 
 const parseInput = input => input.split('\n').map(l => l.split(''))
 
@@ -14,27 +14,21 @@ const countXmas = (r, c, [dr, dc], grid) => {
     return 1
 }
 
-const solve = (isPart2, input) => {
-    input = parseInput(input)
-    let total = 0
-
-    for (let r = 0; r < input.length; r++) {
-        for (let c = 0; c < input[0].length; c++) {
-            if (isPart2) {
-                if (input[r][c] == 'A') {
-                    const corners = getSurroundingGridTiles(input, r, c, [[-1, -1], [-1, 1], [1, -1], [1, 1]])
-                    total += ['MMSS', 'SMSM', 'SSMM', 'MSMS'].includes(corners.join('')) ? 1 : 0
-                }
-            } else {
-                for (const delta of eightWayDeltas) total += countXmas(r, c, delta, input)
+const solve = (isPart2, input) => gridCells(input)
+    .reduce((total, {row, col, value}) => {
+        if (isPart2) {
+            if (value == 'A') {
+                const corners = getSurroundingGridTiles(input, row, col, [[-1, -1], [-1, 1], [1, -1], [1, 1]])
+                total += ['MMSS', 'SMSM', 'SSMM', 'MSMS'].includes(corners.join('')) ? 1 : 0
             }
+        } else {
+            for (const delta of eightWayDeltas) total += countXmas(row, col, delta, input)
         }
-    }
-    return total
-}
+        return total
+    }, 0)
 
-const part1 = input => solve(false, input)
+const part1 = input => solve(false, parseInput(input))
 
-const part2 = input => solve(true, input)
+const part2 = input => solve(true, parseInput(input))
 
 module.exports = { part1, part2 }
