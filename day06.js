efcc51f9c7da1d95435f326546ec6d2fdc9498e6
Manuel Sgobino <msgobino@netcologne.de>
2024-12-10 21:08:41 +0100

    day 06: optimization for part 2 (use the path of part 1)

--- day06.js
+++ day06.js
@@ -5,46 +5,45 @@ const { fourWayDeltas, validCoordForGrid, getSurrounding } = require('./utils.js
 const parseInput = input => input.split('\n').map(l => l.split(''))
 
 const getStartParams = input => input.reduce((pos, line, r) => {
-        const c = line.join('').search(/[v<>^]/g)
+    const c = line.join('').search(/[v<>^]/g)
 	return c != -1 ? [r, c, '^>v<'.indexOf(line[c])] : pos
-    }, [-1, -1, -1])
+}, [-1, -1, -1])
 
 const walk = (grid, r, c, d) => {
     const state = new Set(), visited = new Set()
     while (validCoordForGrid(r, c, grid)) {
-        if (state.has(`${r},${c},${d}`)) return [visited.size, 1]
+        if (state.has(`${r},${c},${d}`)) return [visited, 1]
         state.add(`${r},${c},${d}`)
         visited.add(`${r},${c}`)
         const nextTiles = getSurrounding(grid, r, c, [fourWayDeltas[d]])
-        if (nextTiles.length == 0) return [visited.size, 0]
+        if (nextTiles.length == 0) return [visited, 0]
         if (nextTiles[0].tile == '#') {
             d = (d + 1) % 4
         } else {
             [r, c] = [nextTiles[0].row, nextTiles[0].col]
         }
     }
-    return [visited.size, 0]
+    return [visited, 0]
 }
 
 const part1 = input => {
     input = parseInput(input)
     const [r, c, d] = getStartParams(input)
-    return walk(input, r, c, d)[0]
+    return walk(input, r, c, d)[0].size
 }
 
 const part2 = input => {
     input = parseInput(input)
     const [r, c, d] = getStartParams(input)
-    let loops = 0
-    for (let row = 0; row < input.length; row++) {
-        for (let col = 0; col < input[row].length; col++) {
-            if (input[row][col] != '.') continue
+
+    return walk(input, r, c, d)[0]
+        .keys()
+        .map(pos => pos.split(',').map(Number))
+        .reduce((loops, [row, col]) => {
             const grid = input.map(line => line.map(tile => tile))
             grid[row][col] = '#'
-            loops += walk(grid, r, c, d)[1]
-	}
-    }
-    return loops
+            return loops + walk(grid, r, c, d)[1]
+        } , 0)
 }
 
 module.exports = { part1, part2 }
