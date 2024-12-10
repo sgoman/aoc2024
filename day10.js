c4b19a5a64bc10f9377d184d7fa7af32138aec56
Manuel Sgobino <msgobino@netcologne.de>
2024-12-10 08:26:22 +0100

    day 10 cleaned up (whitespace, variable names, saving parameters)

--- day10.js
+++ day10.js
@@ -4,31 +4,33 @@ const { getSurrounding, fourWayDeltas } = require('./utils.js')
 
 const parseInput = input => input.split('\n').map(l => l.split('').map(Number))
 
-const trails = (grid, row, col, target, path, summits, isPart2) => {
-	const steps = getSurrounding(grid, row, col, fourWayDeltas).filter(s => s.tile == target)
-	for (const step of steps) {
-		if (target == 9) {
-			const coord = isPart2 ? [...path, [step.row, step.col]].map(([r, c]) => `${r},${c}`).join(':') : `${step.row},${step.col}`
-			console.log({row, col, target, coord})
-			summits.add(coord)
-		} else {
-			for(const summit of [...trails(grid, step.row, step.col, target + 1, [...path, [step.row, step.col]], summits, isPart2)])
-				summits.add(summit)
-		}
-	}
-	return summits
+const trails = (grid, nextHeight, path, targets, isPart2) => {
+    const [row, col] = path[path.length - 1]
+    const steps = getSurrounding(grid, row, col, fourWayDeltas).filter(s => s.tile == nextHeight)
+    for (const step of steps) {
+        if (nextHeight == 9) {
+            // part 1 targets mountain peaks, part 2 the paths to reach them
+            const coords = isPart2 ? [...path, [step.row, step.col]].map(([r, c]) => `${r},${c}`).join(':') : `${step.row},${step.col}`
+            targets.add(coords)
+        } else {
+            // my version of node does not know about the union method...
+            for(const summit of [...trails(grid, nextHeight + 1, [...path, [step.row, step.col]], targets, isPart2)])
+                targets.add(summit)
+        }
+    }
+    return targets
 }
 
 const solve = (isPart2, input) => {
-	input = parseInput(input)
-	let score = 0
-	for(let row = 0; row < input.length; row++) {
-		for(let col = 0; col < input[row].length; col++) {
-			if (input[row][col] == 0) {
-				score += trails(input, row, col, 1, [[row, col]], new Set(), isPart2).size
-			}
-		}
-	}
+    input = parseInput(input)
+    let score = 0
+    for(let row = 0; row < input.length; row++) {
+        for(let col = 0; col < input[row].length; col++) {
+            if (input[row][col] == 0) {
+                score += trails(input, 1, [[row, col]], new Set(), isPart2).size
+            }
+        }
+    }
     return score
 }
 
