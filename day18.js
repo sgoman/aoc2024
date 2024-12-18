801c69abf98977f0ea5a68ce757ee803f3ca75ba
Manuel Sgobino <msgobino@netcologne.de>
2024-12-18 09:27:31 +0100

    day 18: no need to keep track of the path tiles anymore

--- day18.js
+++ day18.js
@@ -7,15 +7,15 @@ const parseInput = input => input.split('\n').map(l => l.match(/\d+/g).map(Numbe
 const solve = (isPart2, input, bytes, goalRow, goalCol) => {
     const grid = Array.from({length: goalRow + 1}, () => Array(goalCol + 1).fill('.'))
     for (let b = 0; b < bytes; b++) grid[input[b][1]][input[b][0]] = '#'
-    const hash = (r, c) => `${r},${c}`
-    const seen = new Set(), queue = [[0, 0, 0, [[0, 0]]]]
+    const hash = (r, c) => `${r},${c}` // that comma did cost me about 145 minutes...
+    const seen = new Set(), queue = [[0, 0, 0]]
     seen.add(hash(0, 0))
     while (queue.length) {
-        const [r, c, step, path] = queue.shift()
+        const [r, c, step] = queue.shift()
         if (r == goalRow && c == goalCol) return step
         for (const { row, col, tile } of getSurrounding(grid, r, c, [[1, 0], [0, 1], [-1, 0], [0, -1]])) {
             if (!seen.has(hash(row, col)) && tile == '.') {
-                queue.push([row, col, step + 1, [...path, [row, col]]])
+                queue.push([row, col, step + 1])
                 seen.add(hash(row, col))
             }
         }
