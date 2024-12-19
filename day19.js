c0441bbedc1cef72ee955af17a0c51be2f5f940f
Manuel Sgobino <msgobino@netcologne.de>
2024-12-19 07:29:14 +0100

    day 19: cleaned up version

--- day19.js
+++ day19.js
@@ -1,41 +1,26 @@
 'use strict'
 
 const parseInput = input => {
-    let [available, desired] = input.replace(/ /g, '').split('\n\n')
-    return [available.split(','), desired.split('\n')]
+    const [available, desired] = input.split('\n\n')
+    return [available.split(', '), desired.split('\n')]
 }
 
 const solve = (isPart2, [available, desired]) => {
-    available.sort((a, b) => b.length - a.length)
     const cache = new Map()
-    const canBuild = (design, available) => {
+    const waysToBuild = design => {
         if (design.length == 0) return 1
         if (cache.has(design)) return cache.get(design)
-        let count = 0
-        for (const pattern of available.filter(a => design.indexOf(a) == 0)) {
-            count += canBuild(design.replace(pattern, ''), available)
-        }
+        const count = available
+            .filter(a => design.indexOf(a) == 0)
+            .reduce((total, pattern) => total + waysToBuild(design.replace(pattern, '')), 0)
         cache.set(design, count)
         return count
     }
-
-    return desired.reduce((possible, design, i) => {
-        console.log({i, possible, design})
-        if (isPart2) {
-            return possible + canBuild(design, available)
-        } else {
-            return possible + (canBuild(design, available) > 0 ? 1 : 0)
-        }
-    }, 0)
-    return {available, desired}
+    return desired.reduce((possible, design) => possible + (isPart2 ? waysToBuild(design) : ~~(waysToBuild(design) > 0)), 0)
 }
 
-const part1 = input => {
-    return solve(false, parseInput(input))
-}
+const part1 = input => solve(false, parseInput(input))
 
-const part2 = input => {
-    return solve(true, parseInput(input))
-}
+const part2 = input => solve(true, parseInput(input))
 
 module.exports = { part1, part2 }
