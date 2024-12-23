363f4b0749a37384ccc4bb9a42ecd6d7591b33f0
Manuel Sgobino <msgobino@netcologne.de>
2024-12-23 07:43:53 +0100

    day 23: challenge commit for part 2

--- day23.js
+++ day23.js
@@ -9,7 +9,7 @@ const parseInput = input => input.split('\n').reduce((graph, line) => {
     return graph
 }, new Map())
 
-const solve = (isPart2, input) => {
+const getTrios = input => {
     const trios = new Set()
     for (const [node, neighbours] of input) {
         const linked = Array.from(neighbours)
@@ -29,12 +29,33 @@ const solve = (isPart2, input) => {
     return trios.size
 }
 
-const part1 = input => {
-    return solve(false, parseInput(input))
+const enumerate = (graph, keys, groups, excluded, biggest) => {
+    if (keys.size == 0 && excluded.size == 0) {
+        if (groups.size > biggest.size) {
+            biggest.clear()
+            groups.forEach(g => biggest.add(g))
+        }
+        return
+    }
+    for (const k of Array.from(keys)) {
+        groups.add(k)
+        const neighbours = graph.get(k) || new Set()
+        enumerate(graph, new Set([...keys].filter(f => neighbours.has(f))), groups, new Set([...excluded].filter(f => neighbours.has(f))), biggest)
+        groups.delete(k)
+        keys.delete(k)
+        excluded.add(k)
+    }
 }
 
+const part1 = input => getTrios(parseInput(input))
+
 const part2 = input => {
-    return solve(true, parseInput(input))
+    input = parseInput(input)
+    const biggest = new Set()
+    enumerate(input, new Set(input.keys()), new Set(), new Set(), biggest)
+    const password = Array.from(biggest)
+    password.sort()
+    return password.join(',')
 }
 
 module.exports = { part1, part2 }
